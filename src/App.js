d324e4b7e2c23fd9092a316cef048bf8b1948c99
Jabed <jabedahmedrifat1@gmail.com>
2022-11-10 00:05:36 +0600

    component did mount and fetch and else are added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+// import { robots } from './robots';
 import './App.css';
 
 
@@ -10,10 +10,20 @@ class App extends Component{
   constructor(){
   	super()
   	this.state={
-  	  robots: robots,
+  	  robots: [],
   	  searchfield:''
   	 }
    }
+
+
+  componentDidMount() {
+   	fetch('https://jsonplaceholder.typicode.com/users')
+   		.then(response=> response.json())
+   		.then(users => {this.setState({ robots : users})});
+   }
+
+
+
 
    onSearchChange = (event) => {
 	 		this.setState({searchfield: event.target.value})
@@ -24,14 +34,19 @@ class App extends Component{
   	const filteredRobots = this.state.robots.filter(robots =>{
 	    return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
 	})
-  return(
-	  	<div className='tc'>
-		   <h1 className='f1'>RoboFriends</h1>
-		   <SearchBox searchChange={this.onSearchChange}/>
-		   <CardList robots={filteredRobots}/>
-	  	</div>
 
-    );
+  if (this.state.robots.lenth ===0){
+  	return <h1>Loading</h1>
+  } else{
+		  return(
+			  	<div className='tc'>
+				   <h1 className='f1'>RoboFriends</h1>
+				   <SearchBox searchChange={this.onSearchChange}/>
+				   <CardList robots={filteredRobots}/>
+			  	</div>
+
+		    );
+		}
 
 
   }
